eab6d72d4af51298354b86dd3a9020a34350f525
mdyeamin <ahmedyeamin05@gmail.com>
2026-03-08 23:20:20 +0600

    responsive issue solve

--- func/login.js
+++ func/login.js
@@ -1,25 +1,26 @@
 // get the input value and route the home page
 // login form function start
 const login = () => {
-    const getUserNameField = document.getElementById("user-filed");
-    const getPasswordField = document.getElementById("password-filed");
-    const userNameValue = getUserNameField.value.trim();
-    const passwordValue = getPasswordField.value.trim();
-    getUserNameField.value = "";
-    getPasswordField.value = "";
-    
-    if (userNameValue.toLowerCase() === "admin" && passwordValue.toLowerCase() === "admin123") {
-        alert("Login Successfully");
+  const getUserNameField = document.getElementById("user-filed");
+  const getPasswordField = document.getElementById("password-filed");
+  const userNameValue = getUserNameField.value.trim();
+  const passwordValue = getPasswordField.value.trim();
+  getUserNameField.value = "";
+  getPasswordField.value = "";
+
+  if (
+    userNameValue.toLowerCase() === "admin" &&
+    passwordValue.toLowerCase() === "admin123"
+  ) {
     window.location.href = "home.html";
   } else {
-      alert("Wrong username or password");
-    }
+    alert("Wrong username or password");
+  }
 };
 
 // get the login button event
 document.getElementById("login").addEventListener("click", function (event) {
-    event.preventDefault();
-    login();
+  event.preventDefault();
+  login();
 });
 // login form function end
-
